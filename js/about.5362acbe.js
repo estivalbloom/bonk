36f7c71a5a21329cd15ae0ca2b8bd935f4814503
estivalbloom <158510343+estivalbloom@users.noreply.github.com>
2024-08-13 03:46:39 +0000

    Deploying to gh-pages from @ estivalbloom/bonk@ba61a5dd28a590863d65ee4f131eb720db8698a3 🚀

--- js/about.5362acbe.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkbonk_idle"]=self["webpackChunkbonk_idle"]||[]).push([[594],{5609:function(n,u,e){e.r(u),e.d(u,{default:function(){return k}});var t=e(6768);const a={class:"about"},r=(0,t.Lk)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,t.uX)(),(0,t.CE)("div",a,s)}var i=e(1241);const o={},l=(0,i.A)(o,[["render",c]]);var k=l}}]);
-//# sourceMappingURL=about.5362acbe.js.map
