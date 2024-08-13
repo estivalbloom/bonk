5c54f6d389c10f1813e2578787cd98afc1171175
estivalbloom <cogmonocle@gmail.com>
2024-08-12 22:32:19 -0400

    Reconfigure project for pages hosting.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const baseUrl = '/bonk'
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: baseUrl,
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: '404.html',
+        template: 'src/assets/404.html',
+        templateParameters: {
+          BASE_URL: '/bonk'
+        }
+      })
+    ]
+  }
 })
